36d84c454d85813cdb0eb73699ef2c1295ece6ab
yuki-matsumura <matsumura@MP01.local>
2019-04-09 00:09:32 +0900

    modify case and case.

--- roman_holiday.py
+++ roman_holiday.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 import sqlite3
-
 now = datetime.datetime.now()
 
 ##### Input number #####
@@ -11,6 +10,13 @@ def cnsl():
   now_ymd = "{0:%Y/%m/%d}".format(now)
   print("Today" + "          " + now_ymd + "(" + now_dow + ")" + "\n" + "\n")
   print("1)show holiday list  2)add holiday  3)del holiday  4)Exit")
+
+  global result
+  result = os.path.exists('holidaylist.db')
+  if result == False:
+    print("+===========================================================+")
+    print("| Database file is not exists. First initialize push '0504' |")
+    print("+===========================================================+")
   global num
   num = input("Please select Number: ")
 
@@ -33,40 +39,71 @@ def action():
 
 ##### Show holiday #####
 def show():
-  os.system('clear')
-  conn = sqlite3.connect("holidaylist.db")
-  c = conn.cursor()
-  select = 'SELECT * FROM TEST'
-  for row in c.execute(select):
-    print row[0]
+  if result == True:
+    os.system('clear')
+    conn = sqlite3.connect("holidaylist.db")
+    c = conn.cursor()
+    select = 'SELECT * FROM daylist'
+    for row in c.execute(select):
+      print row[0]
+  elif result == False:
+    exists()
 
 
 ##### Add holiday #####
 def add():
-  print("add")
-
+### refact start ####
+  if result == True:
+    os.system('clear')
+    conn = sqlite3.connect("holidaylist.db")
+    c = conn.cursor()
+    select = 'SELECT * FROM daylist'
+    for row in c.execute(select):
+      print row[0]
+### refact end ###
+  elif result == False:
+    exists()
 
 ##### Del holiday #####
 def delete():
-  print("delete")
+### refact start ####
+  if result == True:
+    os.system('clear')
+    conn = sqlite3.connect("holidaylist.db")
+    c = conn.cursor()
+    select = 'SELECT * FROM daylist'
+    for row in c.execute(select):
+      print row[0]
+### refact end ###
+  elif result == False:
+    exists()
 
 
 ##### Initialize database #####
 def init():
-### refact start ###
   result = os.path.exists('holidaylist.db')
   if result == True:
     os.system('clear')
-    print("Database file is exists.")
+    print('+==========================+')
+    print('| Database file is exists. |')
+    print('+==========================+')
   elif result == False:
     os.system('clear')
     conn = sqlite3.connect("holidaylist.db")
     c = conn.cursor()
     create = 'CREATE TABLE daylist(doy varchar(10), dayy datetime)'
     c.execute(create)
-    print('Initialize holiday list database.')
-### refact end ###
+    print('+===============================+')
+    print('| Create file "holidaylist.db". |')
+    print('+===============================+')
 
+
+##### Database file is not exists #####
+def exists():
+  os.system('clear')
+  print('+==============================+')
+  print('| Database file is not exists. |')
+  print('+==============================+')
 
 
 cnsl()
