4cdd147ae9be5ddcdd9321321a92c3b95ee340b5
yuki-matsumura <matsumura@MP01.local>
2019-04-21 21:36:20 +0900

    Ver1.0

---
+++ README.md
@@ -0,0 +1,81 @@
+Roman Holiday
+====
+Ver1.0
+- - -
+Table of contents
+----
+* [Overview](#chap1)
+* [Environment](#chap2)
+* [Usage](#chap3)
+* [Details](#chap4)
+* [Remarks](#chap5)
+
+
+<a id="chap1"></a>
+<a href="#chap1"></a>
+- - -
+Overview
+----
+This Python script is an aid to Linux cron. This script is effective when skipping a specific date (holiday). How to use is very easy!
+
+
+<a id="chap2"></a>
+<a href="#chap2"></a>
+- - -
+Environment
+----
+- Linux OS or MacOS
+- Python 2.7.10 or later
+- SQLite3
+
+
+<a id="chap3"></a>
+<a href="#chap3"></a>
+- - -
+Usage
+----
+1. Register a day (holiday) where you do not want to run cron. First, move to the directory where you placed the script.<br>
+`cd <script directory>`<br><br>
+Launch the script "Roman Holiday".<br>
+`python roman_holiday.py`<br>
+
+1. A message appears on the screen instructing you to create a database. This is displayed because there is no database for registering holidays. Follow the instructions on the screen and enter `0504`. `holidaylist.db` is created.
+
+1. Select `2)add holiday` on the initial screen of the script. Follow the script guide to register holidays. Next, enter a description of the holiday.
+
+1. Record the command you want to execute in `/etc/crontab`.<br>
+_Example_<br>
+`0 5 * * * root python anne.py sh /home/script/Backup.sh`
+
+1. cron skips the holiday run. You are relieved of the task of manually stopping script execution. You will have a peaceful holiday.
+
+
+<a id="chap4"></a>
+<a href="#chap4"></a>
+- - -
+Details
+----
+1. When `anne.py` is executed, the result is described in `log/roman_holiday.log`. `Execution time/Execution content/Execution result` is described. If you skip a holiday, a `Holiday Description` will be written.
+
+1. When confirming the registered holiday.<br>
+Select `1)show holiday list`. All registered holidays will be displayed.
+
+1. If you accidentally register a holiday, you can delete it.<br>
+Run `python roman_holiday.py` and select `3)del holiday`. Please enter a date to delete.
+
+1. You can delete past holidays at once.<br>
+Selecting `8)remove old date` will remove all holidays that have passed.
+
+1. If you do not need a break on your computer.<br>
+You can remove all holidays from your computer by selecting `9)remove all date`.
+
+
+<a id="chap5"></a>
+<a href="#chap5"></a>
+- - -
+Remarks
+----
+- If you accidentally delete the database file, run `python roman_holiday.py` to create a database file.
+- Do not change the placement of scripts or database files. The script will stop working.
+- If you find a bug, please report it. I also accept requests for improvements and new ideas. [issue](https://github.com/Yuki-Matsumura/roman_holiday/issues)
+- The movie "Roman Holiday" gave me some ideas on creating this script. I am grateful.
--- roman_holiday.py
+++ roman_holiday.py
@@ -1,23 +1,42 @@
+from datetime import date
 import datetime
 import os
 import sqlite3
-now = datetime.datetime.now()
+
+
 
 ##### Input number #####
 def cnsl():
+  global now
+  now = datetime.datetime.now()
   os.system('clear')
   now_dow = "{0:%a}".format(now)
   now_ymd = "{0:%Y/%m/%d}".format(now)
-  print("Today" + "          " + now_ymd + "(" + now_dow + ")" + "\n" + "\n")
+  print("+==============================================================+" + '\n' +
+        "|                                 | Today" + " | " + now_ymd + " (" + now_dow + ")   |" + "\n" +
+        "+==============================================================+" + "\n" + "\n")
   global result
   result = os.path.exists('holidaylist.db')
   if result == False:
     dbnotexists()
-  print("+=====================+===============+===============+========+")
-  print("| 1)show holiday list | 2)add holiday | 3)del holiday | 4)Exit |")
-  print("+=====================+===============+===============+========+")
+  print("+=====================+===============+===============+========+" + '\n' +
+        "| 1)show holiday list | 2)add holiday | " + color.RED + "3)del holiday" + color.END +
+                                                             " | 4)Exit |" + '\n' +
+        "+=====================+===============+===============+========+" + '\n' +
+        "|  " + color.RED + "8)remove old date" + color.END + "  | " + color.RED + "9)remove all date" + color.END +
+        "                      |" + '\n' +
+        "+==============================================================+")
   global num
-  num = input(" Please select Number: ")
+  try:
+    num = input(" Please select Number: ")
+    action()
+  except:
+    print("+==============================================================+" + '\n' +
+          "|                        Please Number.                        |" + '\n' +
+          "+==============================================================+")
+    raw_input()
+    cnsl()
+##### Input number #####
 
 
 ##### Chose case #####
@@ -33,10 +52,19 @@ def action():
     print("+==============================================================+" + '\n' +
           "|                          See you.                            |")
     bye()
+  elif num == 8:
+    olddate()
+  elif num == 9:
+    alldate()
   elif num == 0504:
     init()
   else:
-    print("It is out of range.")
+    print("+==============================================================+" + '\n' +
+          "|                      It is out of range.                     |" + '\n' +
+          "+==============================================================+")
+    raw_input()
+    cnsl()
+##### Chose case #####
 
 
 ##### Show holiday #####
@@ -45,41 +73,204 @@ def show():
     os.system('clear')
     conn = sqlite3.connect("holidaylist.db")
     c = conn.cursor()
-    select = 'SELECT * FROM dl'
+    select = 'SELECT * FROM dl ORDER BY day'
+    print("+==============================================================+" + '\n' +
+          "|                List of registered holidays.                  |" + '\n' +
+          "+==============================================================+")
     for row in c.execute(select):
-      print(str(row[0]) + " /" + row[1] + " :" + row[2])
-    bye()
+      print(str(row[0])[0:4] + "/"
+         + str(row[0])[4:6] + "/"
+         + str(row[0])[6:8] + " ("
+         + row[1] + ")" + " :" + row[2])
+    conn.close()
+    print("+==============================================================+" + '\n' +
+          "|              Please push enter. Go to manu.                  |" + '\n' +
+          "+==============================================================+")
+    raw_input()
+    cnsl()
   elif result == False:
     exists()
+##### Show holiday #####
 
 
 ##### Add holiday #####
 def add():
-### refact start ####
   if result == True:
     os.system('clear')
-    conn = sqlite3.connect("holidaylist.db")
-    c = conn.cursor()
-    select = 'SELECT * FROM dl'
-    for row in c.execute(select):
-      print row[0]
-### refact end ###
+    print("+==============================================================+" + '\n' +
+          "|                          Add holiday                         |" + '\n' +
+          "+==============================================================+")
+    inyear  = input(" Please enter year  : ")
+    inmonth = input(" Please enter month : ")
+    inday   = input(" Please enter day   : ")
+    try:
+      indate = str(inyear) + str(inmonth).zfill(2) + str(inday).zfill(2)
+      wkday = (datetime.datetime(inyear, inmonth, inday)).strftime('%a')
+      desc = raw_input('Description(Within 50 characters) :  ')
+      conn = sqlite3.connect("holidaylist.db")
+      c = conn.cursor()
+      c.execute("INSERT INTO dl(day, dotw, desc) VALUES(?, ?, ?)", [indate, wkday, desc])
+      conn.commit()
+      conn.close()
+      print("+==============================================================+" + '\n' +
+            "|           Add holiday: " + str(indate)[0:4] + "/" +
+                                          str(indate)[4:6] + "/" +
+                                          str(indate)[6:8] +
+                                " (" + wkday + ")" + "                      |" + '\n' +
+            "+==============================================================+")
+      raw_input()
+      cnsl()
+    except ValueError:
+      print("+==============================================================+" + '\n' +
+            "|                  There is no such date.                      |" + '\n' +
+            "+==============================================================+")
+      raw_input()
+      cnsl()
   elif result == False:
     exists()
+##### Add holiday #####
+
 
 ##### Del holiday #####
 def delete():
-### refact start ####
   if result == True:
     os.system('clear')
-    conn = sqlite3.connect("holidaylist.db")
-    c = conn.cursor()
-    select = 'SELECT * FROM dl'
-    for row in c.execute(select):
-      print row[0]
-### refact end ###
+    now_dow = "{0:%a}".format(now)
+    now_ymd = "{0:%Y/%m/%d}".format(now)
+    print(color.RED + "+==============================================================+" + '\n' +
+                      "|                                 | Today" + " | " + color.END +
+          color.GREEN +  now_ymd + " (" + now_dow + ")" + color.END + color.RED + "   |" + '\n' +
+                      "+==============================================================+" + '\n' +
+                      '|          Delete holiday. Please enter the date.              |' + '\n' +
+                      '+==============================================================+' + color.END)
+    delyear  = input(" Please enter delete year  : ")
+    delmonth = input(" Please enter delete month : ")
+    delday   = input(" Please enter delete day   : ")
+    try:
+      date(int(delyear), int(delmonth), int(delday))
+      deldate = str(delyear) + str(delmonth).zfill(2) + str(delday).zfill(2)
+      wkday = (datetime.datetime(delyear, delmonth, delday)).strftime('%a')
+      conn = sqlite3.connect("holidaylist.db")
+      c = conn.cursor()
+      c.execute("DELETE FROM dl WHERE day =?", [(deldate)])
+      conn.commit()
+      conn.close()
+      print(color.RED + "+==============================================================+" + '\n' +
+                        "|           Del holiday: " + str(deldate)[0:4] + "/" +
+                                                      str(deldate)[4:6] + "/" +
+                                                      str(deldate)[6:8] +
+                                            " (" + wkday + ")" + "                      |" + '\n' +
+                        "+==============================================================+" + color.END)
+      raw_input()
+      cnsl()
+    except ValueError:
+      print("+==============================================================+" + '\n' +
+            "|                  There is no such date.                      |" + '\n' +
+            "+==============================================================+")
+      raw_input()
+      cnsl()
   elif result == False:
     exists()
+##### Del holiday #####
+
+
+##### Del old date #####
+def olddate():
+  if result == True:
+    os.system('clear')
+    y_day = now - datetime.timedelta(days=1)
+    del_day = "{0:%Y/%m/%d}".format(y_day)
+    d_day = str("{0:%Y%m%d}".format(y_day))
+    print(color.RED + '+==============================================================+' + '\n' +
+                      '|              Delete old holiday. Is it OK?                   |' + '\n' +
+                      '+==============================================================+' + '\n' +
+                      '| ' + color.END + color.GREEN + '1) No. I stop erasing data.' + color.END +
+                                       color.RED + ' | 2) Yes. I remove old holiday.  |' + '\n' +
+                      '+==============================================================+' + color.END + '\n' +
+          color.RED + '|                      Befor ' + color.END +
+             color.GREEN + del_day + color.END + color.RED + '                        |' + '\n' +
+                      '+==============================================================+' + color.END)
+    try:
+      adnum = input(" Please select Number: ")
+      if adnum == 1:
+        print("+==============================================================+" + '\n' +
+              "|      Processing has been canceled. Return to the menu.       |" + '\n' +
+              "+==============================================================+")
+        raw_input()
+        cnsl()
+      elif adnum == 2:
+        conn = sqlite3.connect("holidaylist.db")
+        c = conn.cursor()
+        c.execute('DELETE FROM dl WHERE day <=?',[(d_day)])
+        conn.commit()
+        conn.close()
+        print(color.RED + "+==============================================================+" + '\n' +
+                          "|                Deleted old registered dates.                 |" + '\n' +
+                          "+==============================================================+" + color.END)
+        raw_input()
+        cnsl()
+      else:
+        print("+==============================================================+" + '\n' +
+              "|                      It is out of range.                     |" + '\n' +
+              "+==============================================================+")
+        raw_input()
+        cnsl()
+    except:
+      print("+==============================================================+" + '\n' +
+            "|                        Please Number.                        |" + '\n' +
+            "+==============================================================+")
+      raw_input()
+      cnsl()
+  elif result == False:
+    exists()
+##### Del old date #####
+
+
+##### Del all date #####
+def alldate():
+  if result == True:
+    os.system('clear')
+    print(color.RED + '+==============================================================+' + '\n' +
+                      '|              Delete all holiday. Is it OK?                   |' + '\n' +
+                      '+==============================================================+' + '\n' +
+                      '| ' + color.END + color.GREEN + '1) No. I stop erasing data.' + color.END +
+                                       color.RED + ' | 2) Yes. I remove all holiday.  |' + '\n' +
+                      '+==============================================================+' + color.END)
+    try:
+      adnum = input(" Please select Number: ")
+      if adnum == 1:
+        print("+==============================================================+" + '\n' +
+              "|      Processing has been canceled. Return to the menu.       |" + '\n' +
+              "+==============================================================+")
+        raw_input()
+        cnsl()
+      elif adnum == 2:
+        conn = sqlite3.connect("holidaylist.db")
+        c = conn.cursor()
+        alldel = 'DELETE FROM dl'
+        c.execute(alldel)
+        conn.commit()
+        conn.close()
+        print(color.RED + "+==============================================================+" + '\n' +
+                          "|                Deleted all registered dates.                 |" + '\n' +
+                          "+==============================================================+" + color.END)
+        raw_input()
+        cnsl()
+      else:
+        print("+==============================================================+" + '\n' +
+              "|                      It is out of range.                     |" + '\n' +
+              "+==============================================================+")
+        raw_input()
+        cnsl()
+    except:
+      print("+==============================================================+" + '\n' +
+            "|                        Please Number.                        |" + '\n' +
+            "+==============================================================+")
+      raw_input()
+      cnsl()
+  elif result == False:
+    exists()
+##### Del all date #####
 
 
 ##### Initialize database #####
@@ -102,7 +293,8 @@ def init():
                  '|              Create file "holidaylist.db".                   |' + '\n' +
                  '+==============================================================+')
     raw_input()
-    os.system('clear')
+    cnsl()
+##### Initialize database #####
 
 
 ##### Database file is not exists #####
@@ -112,7 +304,8 @@ def exists():
                '|                Database file is not exists.                  |' + '\n' +
                '+==============================================================+')
   raw_input()
-  os.system('clear')
+  cnsl()
+##### Database file is not exists #####
 
 
 ##### Note #####
@@ -121,6 +314,7 @@ def dbnotexists():
         "|                           Note !                             |" + '\n' +
         "+==============================================================+" + '\n' +
         "|  Database file is not exists. First initialize push '0504'   |")
+##### Note #####
 
 
 ##### exit? #####
@@ -129,7 +323,15 @@ def bye():
   raw_input(   '|                 Please push Enter key:                       |' + '\n' +
                '+==============================================================+')
   os.system('clear')
+##### exit? #####
+
+
+##### Color #####
+class color:
+  RED = '\033[31m'
+  GREEN = '\033[32m'
+  END = '\033[0m'
+##### Color #####
 
 
 cnsl()
-action()
